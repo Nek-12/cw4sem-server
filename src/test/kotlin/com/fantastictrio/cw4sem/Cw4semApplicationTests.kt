8f01e7767f7d050e031da364f67adaee6d0b7f9f
Jornwer <betanovdmitry946@gmail.com>
2021-04-19 12:32:07 +0300

    Added source directory in pom.xml

---
+++ src/test/kotlin/com/fantastictrio/cw4sem/Cw4semApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fantastictrio.cw4sem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Cw4semApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
