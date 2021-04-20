c574585ad10e58daae858da0a1c12c88dae2cdf1
Jornwer <betanovdmitry946@gmail.com>
2021-04-20 17:03:14 +0300

    Added swagger

---
+++ src/main/kotlin/com/fantastictrio/cw4sem/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.fantastictrio.cw4sem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket redditCloneApi() {
+        return new Docket(SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Coursework 4 Semester Server")
+                .version("1.0")
+                .description("API for Coursework 4 Semester Server")
+                .build();
+    }
+}
