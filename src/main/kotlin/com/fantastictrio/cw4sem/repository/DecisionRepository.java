6a636da98b91ee76e3437f73e47639b5fb78301f
Jornwer <betanovdmitry946@gmail.com>
2021-05-08 19:36:09 +0300

    Add find decisions by organizationId method

--- src/main/kotlin/com/fantastictrio/cw4sem/repository/DecisionRepository.java
+++ src/main/kotlin/com/fantastictrio/cw4sem/repository/DecisionRepository.java
@@ -2,6 +2,11 @@ package com.fantastictrio.cw4sem.repository;
 
 import com.fantastictrio.cw4sem.model.Decision;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface DecisionRepository extends JpaRepository<Decision, Integer> {
+import java.util.List;
+
+public interface DecisionRepository extends JpaRepository<Decision, Integer>  {
+   // @Query("select d from decision d where organization.id = :id")
+    List<Decision> findByOrganizationId(int id);
 }
